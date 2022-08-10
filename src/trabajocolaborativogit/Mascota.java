d1b2b16e6d640aa3bfb6c67a6a7b42f73fb2b9c1
Joseph Carazo <jocarazope@est.utn.ac.cr>
2022-08-09 22:35:59 -0600

    Se programa, por ahorita, en la clase Mascota, sus atributos y métodos Setter y Getters. También, se elimina la clase Persona.

--- src/trabajocolaborativogit/Mascota.java
+++ src/trabajocolaborativogit/Mascota.java
@@ -9,5 +9,49 @@ package trabajocolaborativogit;
  * @author Fabricio
  */
 public class Mascota {
-    
+
+    private String nombre;
+    private int edad;
+    private int aburrimiento;
+    private int hambre;
+    private int energia;
+    private int necesidades;
+    private boolean morir;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getAburrimiento() {
+        return aburrimiento;
+    }
+
+    public int getHambre() {
+        return hambre;
+    }
+
+    public int getEnergia() {
+        return energia;
+    }
+
+    public int getNecesidades() {
+        return necesidades;
+    }
+
+    public boolean isMorir() {
+        return morir;
+    }
+
 }
