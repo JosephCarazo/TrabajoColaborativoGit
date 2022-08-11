70f1f22bdaa5eaf5c8244b17152af3df5e9ab4e4
Joseph Carazo <jocarazope@est.utn.ac.cr>
2022-08-10 18:10:49 -0600

    Se crea la clase Actividades con sus atributos. Estos atributos pueden cambiar. Se analiza la posibilidad de otros atrbutos.

---
+++ src/trabajocolaborativogit/Actividades.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajocolaborativogit;
+
+/**
+ *
+ * @author josep
+ */
+public class Actividades {
+    
+    private int cantidadAburrimiento;
+    private int cantidadEnergia;
+    
+    
+}
