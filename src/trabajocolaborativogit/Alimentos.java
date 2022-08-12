fdd0cad7226c11b8a55ca85470a6cff3dc4f2f1f
Fabricio Herrera <anherreraso@est.utn.ac.cr>
2022-08-12 16:45:29 -0600

    Se crea la clase Alimentos, se añaden atributos y los métodos de acceso

---
+++ src/trabajocolaborativogit/Alimentos.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajocolaborativogit;
+
+/**
+ *
+ * @author Fabricio
+ */
+public class Alimentos {
+    private String nombre;
+    private int calorias;  
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCalorias(int calorias) {
+        this.calorias = calorias;
+    }
+
+    
+    
+    
+    
+    
+}
+
+
+
+
+
