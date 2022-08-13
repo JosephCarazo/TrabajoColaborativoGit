e3a2cfea39cbf61e9f0554d1b59b64d2efd56fe5
Joseph Carazo <jocarazope@est.utn.ac.cr>
2022-08-13 01:49:08 -0600

    Se cambia el nombre de la subclase Fruta por Manzana. Además se le agrega el método constructor y se le asignan sus valores a los atributos heredados.

---
+++ src/trabajocolaborativogit/Manzana.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajocolaborativogit;
+
+/**
+ *
+ * @author Fabricio
+ */
+public class Manzana extends Alimentos {
+
+    public Manzana(String nombre, int calorias) {
+        super(nombre, calorias);
+    }
+    
+    public Manzana(){
+        this.nombre = "Manzana";        
+        this.calorias = 5;
+    }
+   
+    
+}
